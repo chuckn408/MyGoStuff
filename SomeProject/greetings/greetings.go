880e2f866e45ee5cad4ae0d76e0fa7472262da15
Chuck <32836622+chuckn408@users.noreply.github.com>
2024-10-03 21:26:53 +0000

    Finish Compiling- but need need review
    Review::
    -Unit Test
    -Return a random greeting

--- SomeProject/greetings/greetings.go
+++ SomeProject/greetings/greetings.go
@@ -3,29 +3,33 @@ package greetings
 import (
 	"errors"
 	"fmt"
-	"math/rand"
+	"regexp"
+	"testing"
 )
 
-// Hello returns a greeting for the named person.
-func Hello(name string) (string, error) {
-	// If no name was givern, return an error with a message.
-	if name == "" {
-		return name, errors.New("empty name")
+// TestHelloName calls greetings.Hello with a name, checking vald ret val
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Gladys")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %q, nil`, msg, err, want)
 	}
-	// Create a message using a random format.
-	message := fmt.Sprint(randomFormat(), name)
-	return message, nil
 }
 
-// reandomFormat returns one of the set of greeting messages. The returned msg is at random
-func randomFormat() string {
-	// A slice of msg formats
-	formats := []string{
-		"hi, %v. Welcom!",
-		"great to see you, %v!",
-		"Hail, %v! Well met!",
+// TestHelloEmpty calls greetings.Hello with an empty string, checking for an error.
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return name, errors.New("name is empty")
 	}
 
-	// Return a rand sel msg fmt by spec a rnd ind for the slc of fmts.
-	return formats[rand.Intn(len(formats))]
+	message := fmt.Sprintf(randomFormat())
+	return message, nil
 }
