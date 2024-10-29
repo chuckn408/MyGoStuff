fa740ab4c5e426dc256e62ce0c1985d9dd8094a4
Chuck <32836622+chuckn408@users.noreply.github.com>
2024-10-29 15:23:38 -0700

    Squashed commit of the following:
    commit d953d8af93d691be013229c874aec787a7834461
    Author: Chuck <32836622+chuckn408@users.noreply.github.com>
    Date:   Tue Oct 29 15:22:22 2024 -0700
    
        end of Go Module Tutorial
    
    commit abde3053e54278eac70be38ca433e18ca70bad60
    Author: Chuck <32836622+chuckn408@users.noreply.github.com>
    Date:   Tue Oct 29 15:02:02 2024 -0700
    
        "add a test" part 2
    
    commit 414907ba974e1f4c371fc51e4fdbc22dc7aadd78
    Author: Chuck <32836622+chuckn408@users.noreply.github.com>
    Date:   Tue Oct 29 15:00:45 2024 -0700
    
        "add a test" part 1
    
    commit f060c7920316996329e8799fb4b3c95f16793c69
    Author: Chuck <32836622+chuckn408@users.noreply.github.com>
    Date:   Tue Oct 29 14:42:26 2024 -0700
    
        "Rtn Greet Mlt Ppl"
    
    commit 1cea62046e181cfa7c175da27fe9c62b29b6290f
    Author: Chuck <32836622+chuckn408@users.noreply.github.com>
    Date:   Tue Oct 29 14:21:17 2024 -0700
    
        Page 4 "rand greet"
    
    commit b1cd940b5120b55ac39c7e5f064e48feae40cef5
    Author: Chuck <32836622+chuckn408@users.noreply.github.com>
    Date:   Tue Oct 29 14:11:36 2024 -0700
    
        page 3
    
    commit e548b242a56035990586a2d58d08fa45422842bb
    Author: Chuck <32836622+chuckn408@users.noreply.github.com>
    Date:   Tue Oct 29 14:07:51 2024 -0700
    
        Page 2.2
    
    commit a068a88c8b1e3ee2f8e9aafa87bff5ebb7bb96a2
    Author: Chuck <32836622+chuckn408@users.noreply.github.com>
    Date:   Tue Oct 29 14:06:09 2024 -0700
    
        page 2.1
    
    commit 53f95c2a7f26ad5ee84ad4bbd57d8b94604720e6
    Author: Chuck <32836622+chuckn408@users.noreply.github.com>
    Date:   Tue Oct 29 13:58:55 2024 -0700
    
        page 1

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -6,41 +6,33 @@ import (
 	"math/rand"
 )
 
-// Hello returns a greet for named person
 func Hello(name string) (string, error) {
-	// If no name was given, return an error with a message.
 	if name == "" {
 		return name, errors.New("empty name")
 	}
-	// Create a message using a random format.
+
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
 
-// Hellos return a map that associates each named w/ a msg
 func Hellos(names []string) (map[string]string, error) {
 	messages := make(map[string]string)
-	// Loop through the received slice of names, calliing the Hel func to get msg for ea name
 	for _, name := range names {
 		message, err := Hello(name)
 		if err != nil {
 			return nil, err
 		}
-		// In map, assoc the retrieve msg w/ name.
 		messages[name] = message
 	}
 	return messages, nil
 }
 
-// randomFormat returns one of a set of greet msgs. Retrn msg is rand
 func randomFormat() string {
-	// A slice of msg frmts
 	formats := []string{
 		"Hi, %v. Welcome!",
 		"Great to see you, %v!",
 		"Hail, %v! Well met!",
 	}
 
-	// Rtrn one of the msg frmt sel at rndm.
 	return formats[rand.Intn(len(formats))]
 }
