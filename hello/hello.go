b1cd940b5120b55ac39c7e5f064e48feae40cef5
Chuck <32836622+chuckn408@users.noreply.github.com>
2024-10-29 14:11:36 -0700

    page 3

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"greetings/greetings"
+	"log"
 )
 
 func main() {
-	message := greetings.Hello("Glayds")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
